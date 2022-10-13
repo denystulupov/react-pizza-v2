b95cee41206e3feb42ab2e7fb10e548bed5c800c
denystulupov <tulupovdenys@gmail.com>
2022-10-13 13:35:03 +0300

    Components: Header, Categories, Sort, PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React-Pizza-v2</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
