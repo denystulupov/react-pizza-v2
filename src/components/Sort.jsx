90dea01b575fccd6c972b996a258c8a3ea815649
denystulupov <tulupovdenys@gmail.com>
2022-10-16 19:42:16 +0300

    switching categories

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,18 @@
+import React from 'react';
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+
+  const list = ['популярности', 'цене', 'алфавиту'];
+
+  const sortName = list[selected];
+
+  const onClickSelected = (newSelected) => {
+    setSelected(newSelected);
+    setOpen(false);
+  }
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -15,15 +29,28 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>
+          {sortName}
+        </span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+
+      {
+        open &&
+        <div className="sort__popup">
+          <ul>
+            {
+              list.map((item, i) =>
+                <li key={i}
+                  onClick={() => onClickSelected(i)}
+                  className={i === selected ? 'active' : ''}
+                >
+                  {item}
+                </li>)
+            }
+          </ul>
+        </div>
+      }
+
     </div>
   )
 }
