436074afae33b4db804e6d572103771af22b2cd6
denystulupov <tulupovdenys@gmail.com>
2022-10-19 11:49:28 +0300

    #7: skeleton, routes and css.modules

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Sort from '../components/Sort';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://634d19d8acb391d34a93d15b.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+}
+
+export default Home;
