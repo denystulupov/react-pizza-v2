5e04c71f5e27700fda93f76a882f9db35c10c6aa
denystulupov <tulupovdenys@gmail.com>
2022-10-25 12:34:15 +0300

    #10: search + pagination

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 
 function App() {
+  const [searchValue, setSearcValue] = React.useState('');
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearcValue={setSearcValue} />
       <div className="content">
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home searchValue={searchValue} />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import Sort from '../components/Sort';
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-function Home() {
+function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [categoryId, setCategoryId] = React.useState(0);
   const [sortId, setSortId] = React.useState({
     name: 'популярности ↓',
@@ -16,11 +17,12 @@ function Home() {
   });
 
   React.useEffect(() => {
-    let category = categoryId ? 'category=' + categoryId : '';
+    const category = categoryId ? 'category=' + categoryId : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     setIsLoading(true);
     fetch(
-      `https://634d19d8acb391d34a93d15b.mockapi.io/items?${category}&sortBy=${sortId.sortProperty}&order=${sortId.orderType}`,
+      `https://634d19d8acb391d34a93d15b.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortId.sortProperty}&order=${sortId.orderType}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -28,7 +30,19 @@ function Home() {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortId]);
+  }, [categoryId, sortId, searchValue, currentPage]);
+
+  const skeletons = [...new Array(8)].map((_, i) => <Skeleton key={i} />);
+
+  const pizzas = items
+    // локальный поиск по названию
+    // .filter((obj) => {
+    //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
+    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   return (
     <>
@@ -37,11 +51,8 @@ function Home() {
         <Sort selected={sortId} setSelected={setSortId} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination setCurrentPage={setCurrentPage} />
     </>
   );
 }
