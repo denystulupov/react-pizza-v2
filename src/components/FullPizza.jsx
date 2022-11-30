028d65020cbe5cdba1e586b3654a6ffe87abbc1c
denystulupov <tulupovdenys@gmail.com>
2022-11-30 13:50:17 +0200

    #19: useLocation, useParams, Outlet

---
+++ src/components/FullPizza.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const FullPizza = () => {
+  const { id } = useParams();
+  const [pizza, setPizza] = React.useState(null);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(`https://634d19d8acb391d34a93d15b.mockapi.io/items/${id}`);
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы!');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt="" width={300} height={300} />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} грн.</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
